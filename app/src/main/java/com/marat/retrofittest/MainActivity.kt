a76e79af3ac69d8d22a03574db320d240318c6b7
marat <maratnv101@gmail.com>
2022-09-14 23:27:45 +0300

    Added RecyclerView and adapter

--- app/src/main/java/com/marat/retrofittest/MainActivity.kt
+++ app/src/main/java/com/marat/retrofittest/MainActivity.kt
@@ -2,20 +2,32 @@ package com.marat.retrofittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
 import com.marat.retrofittest.data.api.MainViewModel
 import com.marat.retrofittest.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var vm: MainViewModel
+    private lateinit var mainContainer: FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        vm = ViewModelProvider(this)[MainViewModel::class.java]
+        mainContainer = findViewById(R.id.fragment_container_view)
 
+        supportFragmentManager.beginTransaction().add(
+            R.id.fragment_container_view, CharacterListFragment.newInstance()
+        ).commit()
+
+        // 1+1=3
+        vm = ViewModelProvider(this)[MainViewModel::class.java]
 
     }
 }
--- app/src/main/java/com/marat/retrofittest/adapter/RikAdapter.kt
+++ app/src/main/java/com/marat/retrofittest/adapter/RikAdapter.kt
@@ -1,6 +1,38 @@
 package com.marat.retrofittest.adapter
 
-import androidx.recyclerview.widget.ListAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.marat.retrofittest.Character
+import com.marat.retrofittest.Constants
+import com.marat.retrofittest.databinding.ItemCharacterBinding
 
-class RikAdapter:ListAdapter<> {
+class RikAdapter() : RecyclerView.Adapter<RikAdapter.RikHolder>() {
+    private val characterList = mutableListOf<Character>()
+
+    class RikHolder(private val binding: ItemCharacterBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Character) = with(binding) {
+            characterName.text = item.name
+            characterImg.load(item.img)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RikHolder {
+        val inflater = ItemCharacterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RikHolder(inflater)
+    }
+
+    override fun onBindViewHolder(holder: RikHolder, position: Int) {
+        holder.bind(characterList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return characterList.size
+    }
+    fun addTestList(list: List<Character>){
+        characterList.addAll(list)
+    }
 }
