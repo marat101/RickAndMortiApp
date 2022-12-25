912a97ffbe8a2040d9e50506dd85b9dac2e9581e
marat <maratnv101@gmail.com>
2022-12-25 12:28:12 +0300

    Сделал форматирование даты создания персонажа

--- app/src/main/java/com/marat/retrofittest/ui/fragments/detailfragment/DetailInformationFragment.kt
+++ app/src/main/java/com/marat/retrofittest/ui/fragments/detailfragment/DetailInformationFragment.kt
@@ -1,11 +1,15 @@
 package com.marat.retrofittest.ui.fragments.detailfragment
 
 import android.annotation.SuppressLint
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import android.os.Bundle
 import android.transition.TransitionInflater
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.core.view.isVisible
 import coil.load
 import com.marat.retrofittest.data.model.Result
@@ -13,10 +17,12 @@ import com.marat.retrofittest.databinding.FragmentDetailInformationBinding
 import com.marat.retrofittest.ui.base.BaseFragment
 import com.marat.retrofittest.ui.fragments.listfragment.CharacterListFragment
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class DetailInformationFragment : BaseFragment<FragmentDetailInformationBinding>() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -29,15 +35,21 @@ class DetailInformationFragment : BaseFragment<FragmentDetailInformationBinding>
         val args = arguments?.getParcelable<Result>(CharacterListFragment.ITEM_ARGUMENT)
         if (args?.type == "") binding.characterType.isVisible = false
 
+        Log.e("date", args?.created.toString())
+
+
         binding.apply {
             characterImg.transitionName = args?.id.toString()
             characterImg.load(args?.image.toString())
             characterName.text = args?.name
             characterStatus.text = "Status: ${args?.status}"
-            characterSpecies.text = "Scecies ${args?.species}"
+            characterSpecies.text = "Scecies: ${args?.species}"
             characterType.text = "Type: ${args?.type}"
-            characterGender.text = "Gender ${args?.gender}"
-            characterCreationDate.text = "Created ${args?.created}"
+            characterGender.text = "Gender: ${args?.gender}"
+            val dateformatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm", Locale.ROOT)
+            val parsed = dateformatter.parse(args?.created)
+            val formatted = SimpleDateFormat("dd MMMM yyyy года", Locale.forLanguageTag("ru")).format(parsed)
+            characterCreationDate.text = "Created: $formatted"
         }
     }
 
