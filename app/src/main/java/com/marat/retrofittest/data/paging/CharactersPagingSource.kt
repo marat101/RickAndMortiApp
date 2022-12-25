8a380079816fea0ea7a556a103e25509ea066a84
marat <maratnv101@gmail.com>
2022-12-25 18:02:53 +0300

    Сменил di на koin

--- app/src/main/java/com/marat/retrofittest/data/paging/CharactersPagingSource.kt
+++ app/src/main/java/com/marat/retrofittest/data/paging/CharactersPagingSource.kt
@@ -1,19 +1,17 @@
 package com.marat.retrofittest.data.paging
 
-import androidx.paging.PagingSource
-import androidx.paging.PagingState
 import com.marat.retrofittest.data.api.ApiService
 import com.marat.retrofittest.data.model.Character
 
-class CharactersPagingSource(
-    private val api: ApiService
-    private val query: String
-) : PagingSource<Int, Character>() {
-    override fun getRefreshKey(state: PagingState<Int, Character>): Int? {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Character> {
-        TODO("Not yet implemented")
-    }
-}
+//class CharactersPagingSource(
+//    private val api: ApiService
+//    private val query: String
+//) : PagingSource<Int, Character>() {
+//    override fun getRefreshKey(state: PagingState<Int, Character>): Int? {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Character> {
+//        TODO("Not yet implemented")
+//    }
+//}
