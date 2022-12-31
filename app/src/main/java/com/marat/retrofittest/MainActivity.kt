f6e38e60847634cb88b261489e917f82515c77dd
marat <maratnv101@gmail.com>
2022-12-31 19:05:37 +0300

    Исправил вылеты

--- app/src/main/java/com/marat/retrofittest/MainActivity.kt
+++ app/src/main/java/com/marat/retrofittest/MainActivity.kt
@@ -2,11 +2,13 @@ package com.marat.retrofittest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.marat.retrofittest.ui.fragments.listfragment.CharacterListFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (supportFragmentManager.fragments.isEmpty())(
